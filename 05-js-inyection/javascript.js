6463b8379dde034a621d49e8759d65a7b8175481
cdanni91 <cdanni91@gmail.com>
2025-04-09 10:57:58 -0300

    Create project 06 js-practice

---
+++ 05-js-inyection/javascript.js
@@ -0,0 +1,10 @@
+const paragraphContainer = document.querySelector(".content");
+
+const paragraph = document.createElement("p");
+
+paragraph.textContent = "Hola" ;
+
+paragraphContainer.appendChild(paragraph);
+
+
+
