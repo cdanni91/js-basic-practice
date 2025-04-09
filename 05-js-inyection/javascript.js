d508f23025e5d820b02e16a840d07d1fd24f7156
cdanni91 <cdanni91@gmail.com>
2025-04-09 11:24:23 -0300

    Update 05-js-inyection

--- 05-js-inyection/javascript.js
+++ 05-js-inyection/javascript.js
@@ -1,10 +1,39 @@
+/* Paragraph */
+
 const paragraphContainer = document.querySelector(".content");
 
 const paragraph = document.createElement("p");
 
-paragraph.textContent = "Hola" ;
-
+paragraph.style.color = "red";
+paragraph.textContent = "Hey I’m red!";
 paragraphContainer.appendChild(paragraph);
 
+/* h3 */
+
+const h3Element = document.createElement('h3');
+h3Element.style.color = "blue";
+h3Element.textContent = "I’m a blue h3!";
+paragraphContainer.appendChild(h3Element); // Appending to the same container for now
 
 
+
+
+/* div with other elements inside */
+
+const divContainer = document.querySelector(".content");
+const divElement = document.createElement ("div");
+divElement.style.background = "pink";
+divElement.style.border = "3px solid black";
+
+// Create the h1 inside the div
+const h1InDiv = document.createElement("h1");
+h1InDiv.textContent = "I’m in a div";
+divElement.appendChild(h1InDiv);
+
+// Create the p inside the div
+const pInDiv = document.createElement("p");
+pInDiv.textContent = "ME TOO!";
+divElement.appendChild(pInDiv);
+
+// Append the div to the container
+divContainer.appendChild(divElement);
