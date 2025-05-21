3874a47f40a4e2603888f9069b5df7a506410c9d
cdanni91 <cdanni91@gmail.com>
2025-05-21 15:42:28 -0300

    Finish the requested points

---
+++ 04-blastoff/javascript.js
@@ -0,0 +1,30 @@
+
+let i = 10;
+
+while (i >= 0) {
+
+    if (i === 10) {
+        console.log("Commencing countdown");
+    } else if (i === 0) {
+        console.log ("Blastoff");
+    } else {
+        console.log(`${i} seconds to blastoff !`)
+    };
+
+    i-- ;
+
+};
+
+
+
+
+/* 
+const output = document.querySelector('.output');
+output.textContent = "";
+
+// let i = 10;
+
+// const para = document.createElement('p');
+// para.textContent = ;
+// output.appendChild(para);
+ */
